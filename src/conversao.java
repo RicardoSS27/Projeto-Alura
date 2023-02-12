9c765548635e0a43b0d559dd73d9de07af2ebb16
Ricardo Sousa <ricardosousa2709y@gmail.com>
2023-02-12 17:04:28 -0300

    Initial commit

---
+++ src/conversao.java
@@ -0,0 +1,10 @@
+public class conversao {
+    public static void main(String[] args) {
+        double number = 10.9;
+        int valor = (int) number;//Não arredonda
+        System.out.println(valor);
+        float numero = 10.59F;
+        int valor1 = (int) numero;
+        System.out.println(valor1);
+    }
+}
---
+++ src/teste_variaveis.java
@@ -0,0 +1,8 @@
+public class teste_variaveis {
+    public static void main(String[] args){
+        double idade = 37.0F;
+        System.out.println(idade);
+        idade = 40;
+        System.out.println(idade);
+    }
+}
